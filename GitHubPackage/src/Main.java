0e78feb88ff05d85bf195242a2ef0e344ffe63b9
benawam <ismaelauam@gmail.com>
2023-03-13 16:44:53 +0100

    método sumar

--- GitHubPackage/src/Main.java
+++ GitHubPackage/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(sumar(3, 5));
+    }
+
+    public static int sumar(int x, int y){
+        return x + y;
     }
 }
